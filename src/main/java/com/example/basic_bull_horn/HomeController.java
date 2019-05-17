b07cfe0d17552f2c5488ef9bc99b9bdf95e4d4e5
Anthony Valle <valleganthony@gmail.com>
2019-05-17 09:37:29 -0400

    Updated Controller for routes to the form

--- src/main/java/com/example/basic_bull_horn/HomeController.java
+++ src/main/java/com/example/basic_bull_horn/HomeController.java
@@ -1,7 +1,35 @@
 package com.example.basic_bull_horn;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    TweetRepo tweetRepo;
+
+
+    @GetMapping("/add")
+    public String courseForm(Model model){
+        model.addAttribute("tweet",new Tweet());
+        return "form";
+    }
+
+    @PostMapping("/process")
+    public String processForm(@Valid Tweet tweet, BindingResult result){
+        if (result.hasErrors()){
+            return "courseform";
+        }
+        tweetRepo.save(tweet);
+        return "redirect:/";
+    }
+
+
 }
